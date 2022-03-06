6d89b7e7e65e4be7ad48d64f4bc40d45563fffd3
KeLeonRU <82248830+KeLeonRU@users.noreply.github.com>
2022-03-06 17:48:27 -0600

    Modificación de prueba

--- app/src/main/java/com/practicas/MainActivity.kt
+++ app/src/main/java/com/practicas/MainActivity.kt
@@ -8,4 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //COmentario sabloso padre k lo k parce
 }
